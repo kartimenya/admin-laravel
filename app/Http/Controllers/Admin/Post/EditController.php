77ddbcd8c4c0aebb84dd63d1df458746181ead50
Your Name <you@example.com>
2023-04-27 19:31:43 +0300

    PostService

--- app/Http/Controllers/Admin/Post/EditController.php
+++ app/Http/Controllers/Admin/Post/EditController.php
@@ -2,12 +2,13 @@
 
 namespace App\Http\Controllers\Admin\Post;
 
-use App\Http\Controllers\Controller;
+
+use App\Http\Controllers\Admin\Post\BaseController;
 use App\Models\Category;
 use App\Models\Post;
 use App\Models\Tag;
 
-class EditController extends Controller
+class EditController extends BaseController
 {
     public function __invoke(Post $post)
     {
